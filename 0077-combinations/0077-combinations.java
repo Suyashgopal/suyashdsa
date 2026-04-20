b300ffd74c579c86308b5689be3dfee58b0e7992
Suyashgopal <suyashchamoli1@gmail.com>
2026-04-20 15:44:10 +0530

    Time: 309 ms (5.09%), Space: 97.8 MB (92.23%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,32 +1,29 @@
 class Solution {
+
     public List<List<Integer>> combine(int n, int k) {
-        ArrayList<Integer> li = new ArrayList<>();
-        List<List<Integer>> outer= new ArrayList<>();
-        if(n<k){
-            return outer;
-        }
- fun(li,outer,n,k);
- return outer;
         
-        
+         List<List<Integer>> outer= new ArrayList<>();
+         ArrayList<Integer> li= new  ArrayList<>();
+         fun(n,k,li,outer);
+         return outer;
+
     }
-    void fun(  ArrayList<Integer> li,List<List<Integer>> outer, int n,int k){
-        if(li.size()==k){
-            outer.add(new ArrayList<>(li));
+    void fun(int n,int k,   ArrayList<Integer> al,List<List<Integer>> outer ){
+        if(al.size()==k){
+            outer.add(new ArrayList<>(al));
             return;
+
         }
         for(int i=1;i<=n;i++){
-            if(li.contains(i)){
+            if(al.contains(i)){
                 continue;
-                            }
-         if(li.size()>0 && i<li.get(li.size()-1)){
-            continue;
-         }
-          li.add(i);
-
-          fun(li,outer,n,k);
-          li.remove(li.size()-1);
-
+            }
+            if(al.size()>0 && i<=al.get(al.size()-1)){
+                continue;
+            }
+            al.add(i);
+            fun(n,k,al,outer);
+            al.remove(al.size()-1);
         }
     }
 }
