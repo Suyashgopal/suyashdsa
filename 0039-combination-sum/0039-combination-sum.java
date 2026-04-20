c5058b0a7c6f92fe741664766867426d1033946e
Suyashgopal <suyashchamoli1@gmail.com>
2026-04-20 12:40:52 +0530

    Time: 3 ms (33.85%), Space: 45.6 MB (70.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,30 +1,41 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] arr, int t) {
+        Arrays.sort(arr);
 
-List<List<Integer>> outer  = new  ArrayList<>();
- ArrayList<Integer> li = new  ArrayList<>();
- fun(arr,t,0,outer,li); 
-return outer;
+        List<List<Integer>> outer = new ArrayList<>();
+        ArrayList<Integer> li = new ArrayList<>();
+        fun(arr,t,0,li, outer);
+        return outer;
+
+
+
+
+
         
     }
-
-void fun(int[] arr, int t, int idx,List<List<Integer>> outer , ArrayList<Integer> li ){
-    if(idx==arr.length){
-        if(t==0){
-            outer.add(new ArrayList<>(li));
-
+    void fun(int[] arr, int t,int idx, ArrayList<Integer> al, List<List<Integer>> outer){
+        if(idx==arr.length){
+            if(t==0){
+            outer.add(new ArrayList<>(al));  }
+            return;
         }
-        return;}
-    if(arr[idx]<=t){
-        li.add(arr[idx]);
-        fun(arr, t-arr[idx],idx,outer,li);
-        li.remove(li.size()-1);
+
+            if(arr[idx]<= t){
+          al.add(arr[idx]);
+          fun(arr,t-arr[idx],idx,al, outer);
+            al.remove(al.size()-1);
+
+            }
+           
+
+
+        
+
+        
+      
+        fun(arr,t,idx+1,al, outer);
+
+
+
     }
-    
-
-    fun(arr,t, idx+1,outer,li);
-}
-
-
-
 }
