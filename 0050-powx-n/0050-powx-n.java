3ca34fd614508a0cf88432dd24a7a84bf0d65137
Suyash <suyashchamoli1@gmail.com>
2026-05-01 17:29:42 +0530

    Time: 1 ms (10.16%), Space: 48.4 MB (10.82%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+    Long N=  (long) n;
+    if(N<0){
+      x=1/x;
+      N= -N;
+    }
+
+    double res=1;
+    while(N>0){
+        if((N&1)==1){
+            res= res*x;        }
+            x=x*x;
+            N= N>>1;
+
+    }
+     return res;
+
+    }
+}
